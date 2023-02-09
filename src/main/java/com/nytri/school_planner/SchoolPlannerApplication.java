a01e3438ecae1633b83630c90c73af64c8409674
Nytri Vespira <llTheBlankll@gmail.com>
2023-02-09 14:39:59 +0800

    Initial Commit

---
+++ src/main/java/com/nytri/school_planner/SchoolPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.nytri.school_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nytri/school_planner/SchoolPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nytri.school_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
