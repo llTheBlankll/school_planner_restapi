c58594d3b4590edd826def75adb87562d618a843
Nytri Vespira <llTheBlankll@gmail.com>
2023-02-10 14:27:01 +0800

    Added CRU S(search) functinalities

---
+++ src/main/java/com/nytri/school_planner/controllers/ClassroomController.java
@@ -0,0 +1,72 @@
+package com.nytri.school_planner.controllers;
+
+import com.nytri.school_planner.entities.Classroom;
+import com.nytri.school_planner.repositories.ClassroomRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RequestMapping("/api/classroom")
+@CrossOrigin
+@RestController
+public class ClassroomController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final ClassroomRepository classroomRepository;
+
+    public ClassroomController(ClassroomRepository classroomRepository) {
+        this.classroomRepository = classroomRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Classroom> getAllClassrooms() {
+        return this.classroomRepository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Classroom> searchClassroom_by(@RequestParam("by") String by, @RequestParam("value") String value) {
+        if (by.length() > 128 || value.length() > 128) {
+            return Collections.emptyList();
+        }
+
+        if (by.equals("name")) {
+            return this.classroomRepository.findByRoomNameContains(value);
+        } else if (by.equals("level")) {
+            return this.classroomRepository.findByRoomLevelContains(value);
+        }
+
+        return Collections.emptyList();
+    }
+
+    @PutMapping("/merge")
+    public boolean addClassroom(@RequestBody Classroom classroom) {
+        if (this.classroomRepository.existsByRoomName(classroom.getRoomName())) {
+            logger.debug("Classroom with name " + classroom.getRoomName() + " already exists.");
+            return false;
+        }
+
+        this.classroomRepository.save(classroom);
+
+        return true;
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteClassroom(@RequestBody Classroom classroom) {
+        logger.debug("Classroom " + classroom.getRoomName() + " was deleted successfully.");
+        this.classroomRepository.delete(classroom);
+    }
+
+    @DeleteMapping("/delete")
+    public boolean deleteClassroom_byId(@RequestParam("id") Long id) {
+        if (id > Integer.MAX_VALUE) {
+            logger.error("INTEGER EXCEEDED MAX VALUE");
+            return false;
+        }
+
+        this.classroomRepository.deleteById(id.intValue());
+        return true;
+    }
+}
--- src/main/java/com/nytri/school_planner/repositories/ClassroomRepository.java
+++ src/main/java/com/nytri/school_planner/repositories/ClassroomRepository.java
@@ -3,5 +3,12 @@ package com.nytri.school_planner.repositories;
 import com.nytri.school_planner.entities.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
+
+    List<Classroom> findByRoomNameContains(String name);
+    List<Classroom> findByRoomLevelContains(String level);
+
+    boolean existsByRoomName(String room_name);
 }
